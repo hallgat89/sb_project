bd6e023ccaad4174eeea4eac5d58d63e1670fd48
Hallgat Ádám <hallgat89@gmail.com>
2017-12-08 20:32:43 +0100

    Thymeleaf

--- src/main/java/com/hallgat/notes/controller/MainController.java
+++ src/main/java/com/hallgat/notes/controller/MainController.java
@@ -3,13 +3,15 @@ package com.hallgat.notes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @RequestMapping("/")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String index(Model model) {
         return "index";
     }
 
